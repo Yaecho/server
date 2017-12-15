472c146fb3f96ce99ab9c4a46a15d7da907c69d9
yaecho <dz1401@aliyun.com>
2017-12-15 09:29:12 +0800

    cert脚本完善

--- nginx/cert.sh
+++ nginx/cert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-certbot renew
+certbot renew --force-renewal
 
 cp /etc/letsencrypt/live/yaecho.net/fullchain.pem /data/server/ssl/yaecho.net/
 cp /etc/letsencrypt/live/yaecho.net/privkey.pem /data/server/ssl/yaecho.net/
